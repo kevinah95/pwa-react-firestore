4ee157e4981a5346b6623e0b5e00ecf79021380b
Kevin Hernández <kevinah95@gmail.com>
2018-08-08 15:51:21 -0600

    first commit :tada:

---
+++ src/service-worker.js
@@ -0,0 +1,28 @@
+
+// See https://developers.google.com/web/tools/workbox/guides/configure-workbox
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// app-shell
+workbox.routing.registerRoute("/", workbox.strategies.networkFirst());
+
+// webfont-cache
+workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'googleapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ],
+    }),
+  );
